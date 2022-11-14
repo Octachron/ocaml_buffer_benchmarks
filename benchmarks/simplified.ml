41aca167ea2168a7cf0980b9443cfea85a90cdf0
Florian Angeletti <florian.angeletti@inria.fr>
2022-11-14 14:32:31 +0100

    Avoid code duplication

--- benchmarks/simplified.ml
+++ benchmarks/simplified.ml
@@ -1,23 +1,4 @@
 module Buffer= Buffer.Simplified
 let name = "simplified"
 
-
-let char ~reset ~addition =
-  let buf = Buffer.create 1 in
-  for _ = 1 to reset do
-    Buffer.reset buf;
-    for _ = 1 to addition do
-      Buffer.add_char buf 'a';
-    done;
-  done
-
-let string ~reset ~addition =
-  let buf = Buffer.create 1 in
-  for _ = 1 to reset do
-    Buffer.reset buf;
-    for i = 1 to addition do
-      Buffer.add_string buf (Strings.get i);
-    done;
-  done
-
-let benchmark = {Type.name;char;string}
+#include "functor_body.ml"
