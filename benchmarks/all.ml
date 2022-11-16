e29fe6b25abfcb1b84633e5c20dfc59a691d9d99
Florian Angeletti <florian.angeletti@inria.fr>
2022-11-16 11:45:43 +0100

    Test a full indirection version

--- benchmarks/all.ml
+++ benchmarks/all.ml
@@ -4,3 +4,4 @@ let safe = Safe.benchmark
 let safe_nospill = Safe_nospill.benchmark
 let data = Data_safe.benchmark
 let simplified = Simplified.benchmark
+let full_indirection = Full_indirection.benchmark
---
+++ benchmarks/full_indirection.ml
@@ -0,0 +1,4 @@
+module Buffer= Buffers.Full_indirection
+let name = "full_indirection"
+
+#include "functor_body.ml"
